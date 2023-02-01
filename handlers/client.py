3e6904859ac6b35b003bea0d025a3faa5c124302
Ivan Vybornov <99390222+i-vybornov@users.noreply.github.com>
2023-02-01 21:16:58 +0300

    урок Машина состояний

--- handlers/client.py
+++ handlers/client.py
@@ -1,10 +1,11 @@
 from aiogram import types, Dispatcher
 from create_bot import dp, bot
+from keyboards import kb_client
 
 #@dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
     try:
-        await bot.send_message(message.from_user.id, 'Приятного аппетита')
+        await bot.send_message(message.from_user.id, 'Приятного аппетита', reply_markup=kb_client)
         #await message.delete()
     except:
         await message.reply('Общение с ботом через ЛС, напишите ему: \nhttps://t.me/MyFreePracticeBot')
