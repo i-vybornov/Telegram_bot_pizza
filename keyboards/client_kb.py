ba432d689b4256aed1ec711c021dca091ef22979
Ivan Vybornov <99390222+i-vybornov@users.noreply.github.com>
2023-02-01 21:22:23 +0300

    урок машина состояний

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton#, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton  # , ReplyKeyboardRemove
 
 b1 = KeyboardButton('/Режим_работы')
 b2 = KeyboardButton('/Расположение')
@@ -8,6 +8,6 @@ b3 = KeyboardButton('/Меню')
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 
-kb_client.add(b1).add(b2).add(b3)#.row(b4, b5)
-#kb_client.row(b1, b2, b3)
-#kb_client.add(b1).row(b2, b3)
+kb_client.add(b1).add(b2).add(b3)  # .row(b4, b5)
+# kb_client.row(b1, b2, b3)
+# kb_client.add(b1).row(b2, b3)
